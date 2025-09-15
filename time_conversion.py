edfea78749d683b8fb18a1aa3b09045e782bfdc3
Ashwath <nakateashwath@gmail.com>
2025-09-15 10:52:17 +0530

    time conversion

---
+++ time_conversion.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def timeConversion(s):
+    # Write your code here
+    hour = int(s[:2])
+    minutes = s[3:5]
+    seconds = s[6:8]
+    period = s[-2:]
+    
+    if period== "AM":
+        if hour==12:
+            hour=0
+    else:
+        if hour!= 12:
+            hour +=12
+
+    return f"{hour:02}:{minutes}:{seconds}"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
